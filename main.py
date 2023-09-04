992574308a489e2d79c1e93bfb754c35fb8d42a8
BEKIRKSU <bekirkscu@gmail.com>
2023-09-04 19:23:02 +0100

    Too many/little blank lines

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ resources = {
     "coffee": 100,
 }
 
+
 def is_resource_sufficient(order_ingredients):
     """This returns true if ingredients are sufficient and order can be made. False if vv."""
     is_enough = True
@@ -42,6 +43,7 @@ def is_resource_sufficient(order_ingredients):
             is_enough = False
     return is_enough
 
+
 def process_coins():
     """This will return the total from the coins inserted."""
     print("Please insert coins.")
@@ -50,6 +52,7 @@ def process_coins():
     total += int(input("how many nickels?: ")) * 0.05
     total += int(input("how many pennies?: ")) * 0.01
     return total
+
 
 def is_transaction_successful(money_received, drink_cost):
     """Return True when payment is enough/accepted. False if vv."""
@@ -65,16 +68,18 @@ def is_transaction_successful(money_received, drink_cost):
         return False
 #     Return has to be the last thing in the function. If it's before print, print wouldn't be called.
 
+
 def make_coffee(drink_name, order_ingredients):
     """This will deduct the ingredients used from the resources."""
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
     print(f"Here is your {drink_name}")
 
+
 is_on = True
 
 while is_on:
-    choice = input("What would you like? (Espresso/Latte/Cappucino):")
+    choice = input("What would you like? (Espresso/Latte/Cappuccino):")
     if choice == "off":
         is_on = False
     elif choice == "report":
@@ -87,8 +92,4 @@ while is_on:
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
             if is_transaction_successful(payment, drink["cost"]):
-
-
-
-
-
+                make_coffee(choice, drink["ingredients"])
