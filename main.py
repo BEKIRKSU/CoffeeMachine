1ebf7d1c8f0ced636e7d7d8c4d19ce91f4837796
BEKIRKSU <bekirkscu@gmail.com>
2023-09-04 18:47:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,9 +43,10 @@ def is_resource_sufficient(order_ingredients):
 def process_coins():
     print("Please insert coins.")
     total = int(input("how many quarters?: ")) * 0.25
-    total = int(input("how many dimes?: ")) * 0.10
-    total = int(input("how many nickels?: ")) * 0.05
-    total = int(input("how many pennies?: ")) * 0.01
+    total += int(input("how many dimes?: ")) * 0.10
+    total += int(input("how many nickels?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
 
 is_on = True
 
